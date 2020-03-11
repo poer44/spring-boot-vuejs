67acc8c873c050eb48d4edd94bb710c5d12ff5cd
xuebw <Poer666>
2020-03-11 15:29:23 +0800

    美亚版本测试

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,64 +1,60 @@
 <template>
     <div id="app">
-        <el-row>
+        <el-menu
+                class="el-menu-demo"
+                mode="horizontal"
+                background-color="#000"
+                text-color="#fff"
+                active-text-color="#ffd04b">
+            <div class="logodiv">
+                <img src="./assets/pico.png" alt=""/>
+                <span>meiyapico</span>
+            </div>
+        </el-menu>
+        <el-container>
             <el-menu
-                    class="el-menu-demo"
-                    mode="horizontal"
-                    background-color="#000"
-                    text-color="#fff"
+                    default-active="2"
+                    class="left"
+                    @open="handleOpen"
+                    @close="handleClose"
+                    background-color="#f2f2f2"
+                    text-color="#000"
                     active-text-color="#ffd04b">
-                <div class="logodiv">
-                    <img src="./assets/pico.png" alt=""/>
-                    <span>meiyapico</span>
+                <div class="modeldiv">
+                    <img src="./assets/model.png" alt=""/>
+                    <p>模型训练</p>
                 </div>
-            </el-menu>
-        </el-row>
-        <el-container direction="vertical">
-            <el-row>
-                <el-col :span="3" class="leftcol">
-                    <el-menu
-                            default-active="2"
-                            class="left"
-                            @open="handleOpen"
-                            @close="handleClose"
-                            background-color="#f2f2f2"
-                            text-color="#000"
-                            active-text-color="#ffd04b">
-                        <div class="modeldiv">
-                            <img src="./assets/model.png" alt=""/>
-                            <p>模型训练</p>
-                        </div>
-                        <el-menu-item index="2">
-                            <i class="el-icon-menu"></i>
-                            <span slot="title">总览</span>
-                        </el-menu-item>
-                        <el-submenu index="1">
-                            <template slot="title">
-                                <i class="el-icon-location"></i>
-                                <span>训练</span>
-                            </template>
-                            <el-menu-item-group>
-                                <el-menu-item index="1-1">选项1</el-menu-item>
-                                <el-menu-item index="1-2">选项2</el-menu-item>
-                            </el-menu-item-group>
-                        </el-submenu>
+                <el-menu-item index="2">
+                    <i class="el-icon-menu"></i>
+                    <span slot="title">总览</span>
+                </el-menu-item>
+                <el-submenu index="1">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>训练</span>
+                    </template>
+                    <el-menu-item-group>
+                        <el-menu-item index="1-1">选项1</el-menu-item>
+                        <el-menu-item index="1-2">选项2</el-menu-item>
+                    </el-menu-item-group>
+                </el-submenu>
 
-                    </el-menu>
-                </el-col>
-                <el-col :span="21" class="maindiv">
-                    <div id="nav">
-                        <router-link to="/">Hello</router-link>
-                        |
-                        <router-link to="/callservice">Service</router-link>
-                        |
-                        <router-link to="/bootstrap">Bootstrap</router-link>
-                        |
-                        <router-link to="/user">User</router-link>
-                    </div>
-                    <router-view :hellomsg="msg"></router-view>
-                </el-col>
-            </el-row>
+            </el-menu>
+
+            <div id="nav">
+                <router-link to="/">Hello</router-link>
+                |
+                <router-link to="/callservice">Service</router-link>
+                |
+                <router-link to="/bootstrap">Bootstrap</router-link>
+                |
+                <router-link to="/user">User</router-link>
+                <br>
+                <router-view :hellomsg="msg"></router-view>
+            </div>
+
         </el-container>
+
     </div>
 </template>
 
@@ -95,15 +91,9 @@
         margin-left: 20px;
     }
 
-    .left,.leftcol{
-        min-width: 200px;
-    }
-    .maindiv{
-        overflow: hidden;
-    }
-
     .modeldiv {
         text-align: center;
+
         img {
             width: 150px;
             height: 150px;
@@ -115,4 +105,13 @@
         }
     }
 
+    .left {
+        min-width: 200px;
+        height: 100%;
+    }
+
+    html, body, #app, .el-container {
+        height: 100%;
+        overflow: hidden;
+    }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import ElementUI from 'element-ui';
@@ -9,7 +10,7 @@ import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false;
 
 // Bootstrap
-// Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 Vue.use(ElementUI);
 
 new Vue({
